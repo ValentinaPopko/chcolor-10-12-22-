0110d3a2b917ea14ff506a75082ad34ccde3e404
Valentina <110343367+ValentinaPopko@users.noreply.github.com>
2022-12-17 13:44:10 +0300

    Add files via upload

---
+++ test/script.js
@@ -0,0 +1,24 @@
+function chBackcolor(red) {
+    document.body.style.background = red;
+}
+function chBackcolor(Orange) {
+    document.body.style.background = Orange;
+}
+function chBackcolor(Yellow) {
+    document.body.style.background = Yellow;
+}
+function chBackcolor(green) {
+    document.body.style.background = green;
+}
+function chBackcolor(SkyBlue) {
+    document.body.style.background = SkyBlue;
+}
+function chBackcolor(Blue) {
+    document.body.style.background = Blue;
+}
+function chBackcolor(Purple) {
+    document.body.style.background = Purple;
+}
+function chBackcolor(white) {
+    document.body.style.background = white;
+}
